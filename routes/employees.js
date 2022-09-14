5ad63009023fceedcf90c62cf1f84ab805b44c41
ofirshtrosberg <ofiri112@gmail.com>
2022-09-14 16:15:54 +0300

    16:15

--- routes/employees.js
+++ routes/employees.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/', homepage);
 router.post('/addEmp', addEmp);
 router.get('/removeEmp', removeEmp);
-router.get('/editEmp', editEmp);
+router.post('/editEmp', editEmp);
 router.get('/employee', showEmp);
 router.get('/search', searchEmp);
 module.exports = router;
